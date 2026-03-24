5643667684259b6b98185d27bec2cac41db41990
brad <lsihlsl@gmail.com>
2026-03-24 23:08:40 +0900

    resource module 연동

--- src/main/java/com/example/security/controller/UserController.java
+++ src/main/java/com/example/security/controller/UserController.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
     @GetMapping("/profile")
-    public String getProfile(){
-        return "인증 성공";
+    public Map<String, Object> getProfile(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "인증에 성공 하였습니다.");
+        return response;
     }
 }
